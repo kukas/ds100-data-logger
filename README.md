0a4e4b51ccf7c061a2d759973957d8ecde706d8e
Jirka Balhar <balhar.j@gmail.com>
2024-10-16 18:37:45 +0200

    rewrite to support DS100 datalogger

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# Dream-Link-WH1080-Weather-Station
-Python port of Steve Cliffe's perl script that retrieves and logs the weather stats from a WH-1080/1081 weather station
+# USB Temperature And Humidity Data Logger
+
+This is a simple python script to read data from a USB Temperature And Humidity Data Logger.
+
+After connecting the device we see:
+```
+$ lsusb
+...
+Bus 003 Device 029: ID 1941:8021 Dream Link WH1080 Weather Station / USB Missile Launcher
+...
+```
+
+## Installation
+
+The requirements are:
+
+- Python 3
+- pyusb (`pip install pyusb`)
+
+If you get permissions error, you can add a `udev` rule to allow access to the device:
+
+```bash
+sudo nano /etc/udev/rules.d/99-weather-station.rules
+```
+
+Add the Rule:
+
+```
+SUBSYSTEM=="usb", ATTR{idVendor}=="1941", ATTR{idProduct}=="8021", MODE="0666", GROUP="plugdev", SYMLINK+="weather_station"
+```
+
+Make sure that your user is in the `plugdev` group. (`gpasswd -a $USER plugdev`)
+
+Reload `udev` Rules:
+
+```bash
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+```
+
+## Usage
+
+```bash
+$ python3 weather.py
+```
+
+## Device information
+
+### Device names
+
+The device is sold under different names:
+- Datalogger DS100 USB
+- Kongin USB Temperature And Humidity Data Logger
+- KG100 Data Logger
+- FreeTec USB Data Logger v1
+- OTGENKG100BLK-0004728
+- T113
+
+### Specifications
+
+- Memory: Max 16,320 temperature and relative humidity readings
+- Logging Interval: 60 seconds to 4 hours
+- Measure Temperature Range: -40 °Celsius to 60 °Celsius
+- Temperature accuracy: /-1.0 °Celsius under 0-50 °Celsius
+- Measure humidity range: 10%-99%RH
+- Humidity accuracy: /-4% under 20%-80%
+
+## Acknowledgements
+
+The script is based on the following repository:
+
+https://github.com/shaneHowearth/Dream-Link-WH1080-Weather-Station
--- weather.py
+++ weather.py
@@ -1,48 +1,9 @@
-# Model Dreamlink WH1080
 #!/usr/bin/env python
-#
-# This is a python port of 
-# http://www.sjcnet.id.au/wordpress/wp-content/plugins/download-monitor/download.php?id=5
-#
-# If running Ubuntu and getting permissions errors
-# Follow the advice given here http://ubuntuforums.org/showthread.php?t=901891
-#
-# I found the GROUPS portion was REQUIRED on one machine, set it to an appropriate group for
-# your system 
-#
-# /etc/udev/rules.d/41-weather-device.rules
-#
-#
-# SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device",SYSFS{idVendor}=="1941" , SYSFS{idProduct}=="8021", MODE="0666", GROUPS="shane"
-#
-# OTOH my second machine requires these rules:
-#SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device",ATTR{idVendor}=="1941" , ATTR{idProduct}=="8021", MODE="0666", OWNER="shane"
-#
-# Then remove your device from your machine and plug it back in again
-
-# Minimum requirements:
-# - libusb >= 1.0
-# - pyusb >= 1.0.0
-# - python >= 2.6
-
 import usb.core
-import time
 import struct
-import math
-import datetime
-import pause
 
 VENDOR = 0x1941
 PRODUCT = 0x8021
-WIND_DIRS = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW',
-             'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
-max_rain_jump = 10
-previous_rain = 0
-# interval for data collection
-period=1 # minutes
-# Only required for Weather Underground users
-wu_upload_file = "/tmp/wu-wupload.htx"
-
 
 def open_ws():
     '''
@@ -90,185 +51,45 @@ def read_block(device, offset):
                        32)
 
     timeout = 1000  # Milliseconds
-    retval = dev.ctrl_transfer(0x21,  # USB Requesttype
+    retval = device.ctrl_transfer(0x21,  # USB Requesttype
                                0x09,  # USB Request
                                0x200,  # Value
                                0,  # Index
                                tbuf,  # Message
                                timeout)
 
-    return dev.read(0x81, 32, timeout)
+    return device.read(0x81, 32, timeout)
 
-#
-# Return dew point based on temperature & humidity
-#
-# http://en.wikipedia.org/wiki/Dew_Point
-#
-
-
-def dew_point(temperature, humidity):
-    '''
-    Using the supplied temperature and humidity calculate the dew point
-    From Wikipedia: The dew point is the temperature at which the water vapor
-    in a sample of air at constant barometric pressure condenses into liquid
-    water at the same rate at which it evaporates. [1] At temperatures below
-    the dew point, water will leave the air. The condensed water is called dew
-    when it forms on a solid surface. The condensed water is called either fog
-    or a cloud, depending on its altitude, when it forms in the air.
-    @Inputs
-    temperature
-        - float
-    humidity
-        - float
-    @Return dew point
-        - float
-    '''
-    humidity /= 100.0
-    gamma = (17.271 * temperature) / (237.7 + temperature) + math.log(humidity)
-    return (237.7 * gamma) / (17.271 - gamma)
-
-
-#
-# Return wind chill temp based on temperature & wind speed
-#
-# http://en.wikipedia.org/wiki/Wind_chill
-#
-
-def wind_chill(temperature, wind):
-    '''
-    Using the supplied temperature and wind speed calculate the wind chill
-    factor.
-    From Wikipedia: Wind-chill or windchill, (popularly wind chill factor) is
-    the perceived decrease in air temperature felt by the body on exposed skin
-    due to the flow of air
-    '''
-    wind_kph = 3.6 * wind
-
-    # Low wind speed, or high temperature, negates any perceived wind chill
-    if ((wind_kph <= 4.8) or (temperature > 10.0)):
-        return temperature
-
-    wct = 13.12 + (0.6215 * temperature) - \
-        (11.37 * (wind_kph ** 0.16)) + \
-        (0.3965 * temperature * (wind_kph ** 0.16))
-
-    # Return the lower of temperature or wind chill temperature
-    if (wct < temperature):
-        return wct
-    else:
-        return temperature
-
-
-
-############ Open up a connection to the device
+# Open up a connection to the device
 dev = open_ws()
 dev.set_configuration()
 
+i = 0
+data_end = False
+print("Temperature, Humidity")
+while True:
+    # start at offset 256 and read 32 bytes at a time
+    block = read_block(dev, 256+i)
+    # print(block, len(block))
+    for j in range(0, 8):
+        # go through the block 4 bytes at a time
+        block_4bytes = block[j*4:j*4+4]
+        if list(block_4bytes) == [0xFF, 0xFF, 0xFF, 0xFF]:
+            data_end = True
+            break
 
-# Program starts the next round minute from now
-start_time=datetime.datetime.now()+datetime.timedelta(minutes=1)
-start_time=start_time.replace(second=0, microsecond=0)
-sampling_time=start_time
-print('Program started at ' + str(start_time))
-pause.until(start_time)
+        indoor_humidity = block_4bytes[2]
 
-
-try:
-    while True:
-        sampling_time = sampling_time + datetime.timedelta(minutes=period)
-        pause.until(sampling_time)
-        # daily filenames
-        now = str(datetime.datetime.now())
-        filename='/home/pi/Desktop/Data/'+now[0:10]+'-Weather.csv'
-        file = open(filename,'a')
-
-########### Read data
-        # Get the first 32 Bytes of the fixed
-        fixed_block = read_block(dev, 0)
-
-        # Check that we have good data
-        if (fixed_block[0] != 0x55):
-            raise ValueError('Bad data returned')
-
-        # Bytes 31 and 32 when combined create an unsigned short int
-        # that tells us where to find the weather data we want
-        curpos = struct.unpack('H', fixed_block[30:32])[0]
-        current_block = read_block(dev, curpos)
-
-        # Indoor information
-        indoor_humidity = current_block[1]
-        tlsb = current_block[2]
-        tmsb = current_block[3] & 0x7f
-        tsign = current_block[3] >> 7
-        indoor_temperature = (tmsb * 256 + tlsb) * 0.1
-        # Check if temperature is less than zero
+        # temperature is encoded in two bytes
+        tlsb = block_4bytes[1]
+        tmsb = block_4bytes[0] & 0x7f
+        tsign = block_4bytes[0] >> 7
+        indoor_temperature = (tmsb * 256 + tlsb) * 0.1 - 40
         if tsign:
             indoor_temperature *= -1
+        indoor_temperature = round(indoor_temperature, 1)
 
-        # Outdoor information
-        outdoor_humidity = current_block[4]
-        tlsb = current_block[5]
-        tmsb = current_block[6] & 0x7f
-        tsign = current_block[6] >> 7
-        outdoor_temperature = (tmsb * 256 + tlsb) * 0.1
-        # Check if temperature is less than zero
-        if tsign:
-            outdoor_temperature *= -1
-
-        # Bytes 8 and 9 when combined create an unsigned short int
-        # that we multiply by 0.1 to find the absolute pressure
-        abs_pressure = struct.unpack('H', current_block[7:9])[0]*0.1
-        
-        wind = current_block[9]
-        gust = current_block[10]
-        wind_extra = current_block[11]
-        wind_dir = current_block[12]
-        
-        # Bytes 14 and 15  when combined create an unsigned short int
-        # that we multiply by 0.3 to find the total rain
-        # I'm not confident that this is correct. Neither abs_pressure nor
-        # total_rain are returning sane values. In fact total_rain has
-        # stayed static despite rainfall
-        # Looks like I fixed it. They used fixed_block instead of current_block
-        total_rain = struct.unpack('H', current_block[13:15])[0]*0.3
-
-        # Calculate wind speeds
-        wind_speed = (wind + ((wind_extra & 0x0F) << 8)) * 0.38  # Was 0.1
-        gust_speed = (gust + ((wind_extra & 0xF0) << 4)) * 0.38  # Was 0.1
-
-        outdoor_dew_point = dew_point(outdoor_temperature, outdoor_humidity)
-        wind_chill_temp = wind_chill(outdoor_temperature, wind_speed)
-
-        # Calculate rainfall rates
-        if previous_rain == 0:
-            previous_rain = total_rain
-
-        rain_diff = total_rain - previous_rain
-
-        if rain_diff > max_rain_jump:  # Filter rainfall spikes
-            rain_diff = 0
-            total_rain = previous_rain
-
-        previous_rain = total_rain;
-
-############### Save data
-        print(str(datetime.datetime.now())+' Saving data')
-        file.write(str(datetime.datetime.now()))
-        file.write(',')
-        file.write('%i,' %indoor_humidity)
-        file.write('%i,' %outdoor_humidity)
-        file.write('%2.1f,' %indoor_temperature)
-        file.write('%2.1f,' %outdoor_temperature)
-        file.write('%2.2f,' %outdoor_dew_point)
-        file.write('%2.1f,' %wind_chill_temp)
-        file.write('%2.1f,' %wind_speed)
-        file.write('%2.1f,' %gust_speed)
-        file.write('%s,' %WIND_DIRS[wind_dir])
-        file.write('%2.1f,' %rain_diff)
-        file.write('%3.1f,' %total_rain)
-        file.write('%4.1f,' %abs_pressure)
-        file.write('\n')
-        file.close()
-
-except (KeyboardInterrupt, SystemExit):
-    print ('\n...Program Stopped Manually!')
+        print(indoor_temperature, ",", indoor_humidity)
+    if data_end:
+        break
+    i += 32
